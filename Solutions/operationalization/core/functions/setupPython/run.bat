28ed4a7a644d43093c1aa07af1f9f20f2b3a5fe0
Andrew <anivan@microsoft.com>
2018-02-23 16:46:10 -0800

    latest changes

--- Solutions/operationalization/core/functions/setupPython/run.bat
+++ Solutions/operationalization/core/functions/setupPython/run.bat
@@ -1,8 +1,9 @@
 echo OFF
 
-rem TODO: implement nuget.exe discovery so that this works no matter what Kudu version is installed
-"D:\Program Files (x86)\SiteExtensions\Kudu\70.10119.3222\bin\Scripts\nuget.exe" install -Source https://www.siteextensions.net/api/v2/ -OutputDirectory D:\home\site\tools python364x64 
-XCOPY /E /H /Y /C "d:\home\site\tools\python364x64.3.6.4.1\content\python364x64\*" "d:\home\site\tools\"
+for /f "tokens=*" %%i in ('dir "D:\program files (x86)\SiteExtensions\Kudu\" /s /b ^| findstr /i nuget\.exe$') do set NUGET_EXE=%%i
+"%NUGET_EXE%" install -Source https://www.siteextensions.net/api/v2/ -OutputDirectory D:\home\site\tools python364x64 
+
+for /f "tokens=*" %%i in ('dir "D:\home\site\tools\" /b') do set PYTHON_VERSION=%%i
+XCOPY /E /H /Y /C "d:\home\site\tools\%PYTHON_VERSION%\content\python364x64\*" "d:\home\site\tools\"
 
 python.exe -m pip install -r d:\home\site\wwwroot\setupPython\requirements.txt
-
