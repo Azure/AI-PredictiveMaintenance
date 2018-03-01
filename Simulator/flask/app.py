4c15da6a0b7196777556de325529d41783fd026a
Andrew <anivan@microsoft.com>
2018-02-28 17:21:05 -0800

    adding proper breadcrumbs

--- Simulator/flask/app.py
+++ Simulator/flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 import numpy as np
 from threading import Thread
 from azure.storage.blob import PageBlobService
@@ -14,30 +14,47 @@ from gevent import pywsgi, sleep, Timeout
 from geventwebsocket.handler import WebSocketHandler
 from azure.storage.table import TableService, Entity, TablePermissions
 
+from flask_breadcrumbs import Breadcrumbs, register_breadcrumb
+
 # from gevent import monkey
 # monkey.patch_all()
 
 app = Flask(__name__)
 app.debug = True
-sockets = Sockets(app)
+# sockets = Sockets(app)
+
+# Initialize Flask-Breadcrumbs
+Breadcrumbs(app=app)
 
 ACCOUNT_NAME = "stgw74e5yvuyvfs2"
 ACCOUNT_KEY = "CKXt/8SpgY6JHfVwBQUkwBCDeBKyJzQV10sgut3L4dS/bjKlSxQsPedHkrI7td8I0NBWJoQOSkcChGo1MIf9Cw=="
 CONTAINER_NAME = "simulation"
 
-table_service = TableService(account_name=ACCOUNT_NAME, account_key=ACCOUNT_KEY)    
+table_service = TableService(account_name=ACCOUNT_NAME, account_key=ACCOUNT_KEY)
 
 
 @app.route('/')
+@register_breadcrumb(app, '.', 'Home')
 def home():    
     return render_template('home.html')
 
 @app.route('/equipment')
+@register_breadcrumb(app, '.equipment', 'Equipment')
 def equipment():
     assets = table_service.query_entities('equipment')
     return render_template('equipment.html', assets = assets)
 
+
+def view_asset_dlc(*args, **kwargs):
+    kind = request.view_args['kind']
+    tag = request.view_args['tag']
+    return [
+        {'text': kind, 'url': '/equipment/{0}'.format(kind)},
+        {'text': tag, 'url': '/equipment/{0}/{1}'.format(kind, tag)}]
+
+
 @app.route('/equipment/<kind>/<tag>')
+@register_breadcrumb(app, '.equipment.asset', '', dynamic_list_constructor=view_asset_dlc)
 def equipment_asset(kind, tag):
     asset = table_service.get_entity('equipment', kind, tag)
     print(asset)
@@ -51,6 +68,5 @@ if __name__ == "__main__":
 
     asset = {'PartitionKey': 'pm1', 'RowKey': 'pm1-354', 'Installed': datetime(2001, 1, 13), 'Model': 'M009'}
     table_service.insert_or_merge_entity('equipment', asset)
-
 
     app.run('0.0.0.0', 8000, debug=True)
