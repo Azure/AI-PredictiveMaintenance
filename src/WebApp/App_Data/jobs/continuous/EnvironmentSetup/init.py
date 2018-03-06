1c00741bffb449c5f7e3811e407f531b4c8c0585
Andrew <anivan@microsoft.com>
2018-03-06 12:02:02 -0800

    uploading Jupyter notebooks to Azure File Storage

--- src/WebApp/App_Data/jobs/continuous/EnvironmentSetup/init.py
+++ src/WebApp/App_Data/jobs/continuous/EnvironmentSetup/init.py
@@ -1,6 +1,12 @@
 import os
+import urllib
+import zipfile
 from datetime import datetime
 from azure.storage.table import TableService, Entity, TablePermissions
+from azure.storage.blob import BlockBlobService
+from azure.storage.blob import PublicAccess
+from azure.storage.file import FileService
+
 
 STORAGE_ACCOUNT_NAME = os.environ['STORAGE_ACCOUNT_NAME']
 STORAGE_ACCOUNT_KEY = os.environ['STORAGE_ACCOUNT_KEY']
@@ -14,3 +20,26 @@ table_service.insert_or_merge_entity('equipment', asset)
 
 asset = {'PartitionKey': 'pm1', 'RowKey': 'pm1-354', 'Installed': datetime(2001, 1, 13), 'Model': 'M009', 'Speed': 1220}
 table_service.insert_or_merge_entity('equipment', asset)
+
+file_service = FileService(account_name=STORAGE_ACCOUNT_NAME, account_key=STORAGE_ACCOUNT_KEY)
+file_service.create_share(share_name='notebooks', quota=1)
+
+source=os.environ['NOTEBOOKS_URL']
+dest='notebooks.zip'
+
+urllib.request.urlretrieve(source, dest)
+
+with zipfile.ZipFile(dest,"r") as zip_ref:
+    zip_ref.extractall("notebooks")
+
+for root, dirs, files in os.walk('notebooks', topdown=True):
+    directory = os.path.relpath(root, 'notebooks')
+    if directory != '.':
+        file_service.create_directory('notebooks', directory)
+    for f in files:
+        file_service.create_file_from_path(
+            'notebooks',
+            directory,
+            f,
+            os.path.join(root, f))
+
