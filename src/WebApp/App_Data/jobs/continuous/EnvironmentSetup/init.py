1089642652577f8e963d63ed5e06b46fbcc49981
Andrew Ivanov <anivan@microsoft.com>
2018-03-06 07:22:21 -0800

    enabling test devices and sending messages to IoT Hub

--- src/WebApp/App_Data/jobs/continuous/EnvironmentSetup/init.py
+++ src/WebApp/App_Data/jobs/continuous/EnvironmentSetup/init.py
@@ -8,10 +8,9 @@ STORAGE_ACCOUNT_KEY = os.environ['STORAGE_ACCOUNT_KEY']
 table_service = TableService(account_name=STORAGE_ACCOUNT_NAME, account_key=STORAGE_ACCOUNT_KEY)
 
 table_service.create_table('equipment')
-table_service.create_table('workers')
 
-asset = {'PartitionKey': 'pm1', 'RowKey': 'pm1-353', 'Installed': datetime(2009, 10, 10), 'Model': 'M009'}
+asset = {'PartitionKey': 'pm1', 'RowKey': 'pm1-353', 'Installed': datetime(2009, 10, 10), 'Model': 'M009', 'Speed': 1000}
 table_service.insert_or_merge_entity('equipment', asset)
 
-asset = {'PartitionKey': 'pm1', 'RowKey': 'pm1-354', 'Installed': datetime(2001, 1, 13), 'Model': 'M009'}
+asset = {'PartitionKey': 'pm1', 'RowKey': 'pm1-354', 'Installed': datetime(2001, 1, 13), 'Model': 'M009', 'Speed': 1220}
 table_service.insert_or_merge_entity('equipment', asset)
--- src/WebApp/App_Data/jobs/continuous/Simulator/simulator.py
+++ src/WebApp/App_Data/jobs/continuous/Simulator/simulator.py
@@ -2,10 +2,15 @@ import os
 import numpy as np
 import pickle
 import random
+#import concurrent.futures
+from multiprocessing import Pool, TimeoutError, cpu_count
+from multiprocessing.dummy import Pool as DummyPool
+
 from iot_hub import DeviceManager, D2CMessageSender
 from device import Device
 from azure.storage.table import TableService, Entity, TablePermissions
 import datetime
+import time
 
 STORAGE_ACCOUNT_NAME = os.environ['STORAGE_ACCOUNT_NAME']
 STORAGE_ACCOUNT_KEY = os.environ['STORAGE_ACCOUNT_KEY']
@@ -14,56 +19,86 @@ IOT_HUB_NAME = os.environ['IOT_HUB_NAME']
 IOT_HUB_OWNER_KEY = os.environ['IOT_HUB_OWNER_KEY']
 IOT_HUB_DEVICE_KEY = os.environ['IOT_HUB_DEVICE_KEY']
 
-WEBSITE_INSTANCE_ID = os.environ['WEBSITE_INSTANCE_ID']
-
-
 table_service = TableService(account_name=STORAGE_ACCOUNT_NAME, account_key=STORAGE_ACCOUNT_KEY)
 
-asset = {'PartitionKey': WEBSITE_INSTANCE_ID, 'RowKey': datetime.datetime.now().timestamp()}
-table_service.insert_or_merge_entity('equipment', asset)
+ownerConnectionString ='HostName=%s.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=%s' % (IOT_HUB_NAME, IOT_HUB_OWNER_KEY)
+deviceConnectionString = 'HostName=%s.azure-devices.net;SharedAccessKeyName=device;SharedAccessKey=%s' % (IOT_HUB_NAME, IOT_HUB_DEVICE_KEY)
 
-connectionString ='HostName=%s.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=%s' % (IOT_HUB_NAME, IOT_HUB_OWNER_KEY)
+dm = DeviceManager(ownerConnectionString)
 
-assets = table_service.query_entities('equipment')
+sender = D2CMessageSender(deviceConnectionString)
 
-dm = DeviceManager(connectionString)
+def get_target_speed(device):
+    asset = table_service.get_entity('equipment', device.make, device.device_id)        
+    return asset['Speed']
 
-for asset in assets:
-    deviceId = asset.RowKey
-    dm.createDeviceId(deviceId)
-    print(dm.retrieveDeviceId(deviceId))
-
-print(dm.listDeviceIds())
-
-connectionString = 'HostName=%s.azure-devices.net;SharedAccessKeyName=device;SharedAccessKey=%s' % (IOT_HUB_NAME, IOT_HUB_DEVICE_KEY)
-
-sender = D2CMessageSender(connectionString)
-
-def process(device_id, state):
-    samples.append(state['vibration'])
-    # sending iothub message    
-    pl = pickle.dumps(state)
-    print('%s - %s' % (device_id, sender.sendD2CMsg(device_id, pl)))
+def process(device):
+    # sending iothub message
+    pl = pickle.dumps(device.state)
+    return sender.sendD2CMsg(device.device_id, pl)
     #print(state)
 
-devices = []
-for asset in assets:
-    devices.append(
-        Device(asset.RowKey, W = (1, 2, 3, 4, 5, 12, 15), A = (5, 8, 2/3, 9, 8, 13, 5))
-    )
+
+if __name__ == '__main__':
+
+    assets = table_service.query_entities('equipment')
+    
+    for asset in assets:
+        deviceId = asset.RowKey
+        dm.createDeviceId(deviceId)
+        print(dm.retrieveDeviceId(deviceId))
+
+    print(dm.listDeviceIds())
+
+    devices = []
+    for asset in assets:
+        devices.append(
+            Device(asset.RowKey, make=asset.PartitionKey, W = (1, 2, 3, 4, 5, 12, 15), A = (5, 8, 2/3, 9, 8, 13, 5))
+        )
+
+    #devices[3].pressure_factor = 1.5
+    print('CPU count: {0}'.format(cpu_count()))
+    pool = Pool(processes=cpu_count())
+    dummyPool = DummyPool(100)
+
+    async_result = None
+    target_speeds = None
+
+    #for _ in range(19):
+    while True:
+        interval_start = time.time()
+
+        states = None
+
+        if async_result is None:
+            #start = time.time()
+            async_result = dummyPool.map_async(get_target_speed, devices)
+        
+        if target_speeds is None or async_result.ready():
+            target_speeds = async_result.get()
+            async_result = None
+
+        print(target_speeds)
+
+        for device, target_speed in zip(devices, target_speeds):
+            device.set_speed((target_speed + device.get_speed()) / 2)
+
+        start = time.time()
+        states = pool.map(Device.next_state, devices)
+        end = time.time()
+        print(end - start)
+
+        start = time.time()
+        dummyPool.map(process, devices)
+        end = time.time()
+        print(end - start)
 
 
-#devices[3].pressure_factor = 1.5
+        time_elapsed = time.time() - interval_start
+        print('Cadence: {0}'.format(time_elapsed))
+        time.sleep(max(1 - time_elapsed, 0))
 
-for _ in range(1):
-    for device in devices:
-        schedule = (
-            (random.randint(20, 40), random.randint(999, 1100)),
-            (random.randint(20, 40), random.randint(1400, 1600)),
-            (random.randint(3, 10), random.randint(400, 600)),
-            (random.randint(3, 5), 0))
-        samples = []
-        device.simulate_schedule(schedule, process)
-        wave_data = np.concatenate(np.array(samples, dtype=np.int16))
-        from scipy.io.wavfile import write
-        write('test-{0}.wav'.format(device.device_id), 8000, wave_data)
+        # for device, state in zip(devices, states):
+        #     process(device.device_id, state)
+            # from scipy.io.wavfile import write
+            # write('test-{0}.wav'.format(device.device_id), 8000, wave_data)
