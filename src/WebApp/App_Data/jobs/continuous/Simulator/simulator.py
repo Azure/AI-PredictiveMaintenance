9f59834b8c6baf69590a869783cd5f89b5b03f70
Andrew <anivan@microsoft.com>
2018-03-26 21:27:49 -0700

    more devices, 2 types of failures

--- src/WebApp/App_Data/jobs/continuous/Simulator/simulator.py
+++ src/WebApp/App_Data/jobs/continuous/Simulator/simulator.py
@@ -12,34 +12,53 @@ import datetime
 import time
 import json
 
-STORAGE_ACCOUNT_NAME = os.environ['STORAGE_ACCOUNT_NAME']
-STORAGE_ACCOUNT_KEY = os.environ['STORAGE_ACCOUNT_KEY']
+# STORAGE_ACCOUNT_NAME = os.environ['STORAGE_ACCOUNT_NAME']
+# STORAGE_ACCOUNT_KEY = os.environ['STORAGE_ACCOUNT_KEY']
 
-IOT_HUB_NAME = os.environ['IOT_HUB_NAME']
-IOT_HUB_OWNER_KEY = os.environ['IOT_HUB_OWNER_KEY']
-IOT_HUB_DEVICE_KEY = os.environ['IOT_HUB_DEVICE_KEY']
+# IOT_HUB_NAME = os.environ['IOT_HUB_NAME']
+# IOT_HUB_OWNER_KEY = os.environ['IOT_HUB_OWNER_KEY']
+# IOT_HUB_DEVICE_KEY = os.environ['IOT_HUB_DEVICE_KEY']
+
+STORAGE_ACCOUNT_NAME = 'stgd3fcsizbq4vj6'
+STORAGE_ACCOUNT_KEY = '3yqJD70UAdNqeMhWveluzmMWpcHi1SV6RcpFoUqZbL57ppmSdDCMFfqlzHmHuatQksHUGwyh4YC1t7C4+oDLQA=='
+
+IOT_HUB_NAME = 'iothub-d3fcsizbq4vj6'
+IOT_HUB_OWNER_KEY = '9e+8Dj8OWJrI7DZy3dKtxnRaV4RDp77L8SXnVe6w1ok='
+IOT_HUB_DEVICE_KEY = 'c6YH5pD5oybHu8ZtZb8/9rx3Oq9JjqgzfCaOYMZEZ4I='
 
 table_service = TableService(account_name=STORAGE_ACCOUNT_NAME, account_key=STORAGE_ACCOUNT_KEY)
 
-def device_driver(device):
+def device_driver(device_id):
+    global digital_twin
     global target_speed
     
-    iothub_device = IoTHubDevice(IOT_HUB_NAME, device.device_id, IOT_HUB_DEVICE_KEY)
+    iothub_device = IoTHubDevice(IOT_HUB_NAME, device_id, IOT_HUB_DEVICE_KEY)
     target_speed = 0
+    digital_twin = None
 
     def device_twin_callback(update_state, payload, user_context):
+        global digital_twin
         global target_speed
+
         properties_json = json.loads(payload)
         if str(update_state) == 'COMPLETE':
             properties_json = properties_json['desired']
+            spectral_profile = json.loads(properties_json['spectralProfile'])
+            pressure_factor = properties_json['pressureFactor']
+            digital_twin = Device(device_id, make='model1', W = spectral_profile['W'], A = spectral_profile['A'])
+            digital_twin.pressure_factor = pressure_factor
         if 'speed' in properties_json:
             target_speed = properties_json['speed']
 
     iothub_device.client.set_device_twin_callback(device_twin_callback, 0)
 
     while True:
+        if digital_twin == None:
+            time.sleep(1)
+            continue
+        
         interval_start = time.time()
-        state = device.next_state()
+        state = digital_twin.next_state()
         
         if state['speed'] == 0 and target_speed == 0:
             # the device is fully turned off
@@ -49,7 +68,7 @@ def device_driver(device):
         pl = bytearray(pickle.dumps(state))
         iothub_device.send_message(pl)
         
-        device.set_speed((target_speed + device.get_speed()) / 2)
+        digital_twin.set_speed((target_speed + digital_twin.get_speed()) / 2)
 
         iothub_device.send_reported_state({
             'speed': state['speed'],
@@ -63,44 +82,56 @@ def device_driver(device):
         # print('Cadence: {0}'.format(time_elapsed))
         time.sleep(max(1 - time_elapsed, 0))
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     iot_hub = IoTHub(IOT_HUB_NAME, IOT_HUB_OWNER_KEY)
     
+    iot_device_count = 10
+
     devices = iot_hub.get_device_list()
 
     if len(devices) == 0:
         devices = []
-        for i in range(4):
-            device_id = 'M-{0:03d}'.format(i)
+        for i in range(iot_device_count):
+            device_id = 'MACHINE-{0:03d}'.format(i)
             device = iot_hub.create_device(device_id)
             devices.append(device)
+
+    healthy_spectral_profile = {
+        'W': [1, 2, 3, 4, 5, 12, 15],
+        'A': [5, 8, 2/3, 9, 8, 13, 5]
+    }
+
+    rotor_imbalance_speactral_profile = {
+        'W': [1/2, 1, 2, 3, 5, 7, 12, 18],
+        'A': [1, 5, 80, 2/3, 8, 2, 14, 50]
+    }
+
+    rotor_imbalance_device_id = devices[-1].deviceId
+    low_pressure_device_id = devices[-2].deviceId
     
     for device in devices:
         twin_data = iot_hub.get_device_twin(device.deviceId)
         twin_data_json = json.loads(twin_data)
         twin_properties = twin_data_json['properties']
-        if 'speed' not in twin_properties['desired']:            
+        if 'speed' not in twin_properties['desired']:
             twin_properties = {
                 'properties': {
                     'desired': {
-                        'speed': random.randint(600, 1500)
+                        'speed': random.randint(600, 1500),
+                        'spectralProfile': json.dumps(healthy_spectral_profile if device.deviceId != rotor_imbalance_device_id else rotor_imbalance_speactral_profile),
+                        'pressureFactor': 2 if device.deviceId != low_pressure_device_id else 1.5
                     }
                 }
             }
             iot_hub.update_twin(device.deviceId, json.dumps(twin_properties))
 
     processes = []
-    for device in devices:
-        digital_twin = Device(device.deviceId, make='model1', W = (1, 2, 3, 4, 5, 12, 15), A = (5, 8, 2/3, 9, 8, 13, 5))
-        processes.append(Process(target=device_driver, args=(digital_twin, )))
+    for device in devices:        
+        processes.append(Process(target=device_driver, args=(device.deviceId, )))
 
     for process in processes:
         process.daemon = True
         process.start()
-        
+
     while all(map(lambda c: c.is_alive(), processes)):
         time.sleep(3)
-
-    exit(0)
-
-    #devices[3].pressure_factor = 1.5
