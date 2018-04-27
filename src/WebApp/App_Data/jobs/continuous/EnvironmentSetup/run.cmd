e6b5f0e435144bcf4eb2008154915162ee6be058
Andrew Ivanov <andrew.ivanov@live.com>
2018-04-26 21:48:59 -0700

    upgrading AZTK (#61)

--- src/WebApp/App_Data/jobs/continuous/EnvironmentSetup/run.cmd
+++ src/WebApp/App_Data/jobs/continuous/EnvironmentSetup/run.cmd
@@ -9,7 +9,7 @@ D:\home\python364x64\python.exe init.py
 pushd d:\home\site\
 git clone https://www.github.com/azure/aztk
 cd aztk
-git checkout 3cc43c3277dd8a51ba96a606978c745888952d6d
+git checkout 07ac9b7596fae8580ef2743451d29536f5244789
 D:\home\python364x64\python.exe -m pip install -e .
 popd
 echo. > %READY_FILE%
