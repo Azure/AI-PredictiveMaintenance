ac19f332d18e95b37dcc67d8302cc6ee8628196c
Lakshmanan Ramu Meenal <laramume@microsoft.com>
2018-05-04 14:12:12 -0700

    integrate dsvm

--- assets/envsetup.sh
+++ assets/envsetup.sh
@@ -9,11 +9,8 @@ source /anaconda/bin/activate py35
 conda install nbformat
 conda env update --file ~/AML/aml_config/conda_dependencies.yml
 
+pushd ~/notebooks
+rm -rf *
+popd
+
 cp -r ~/AML/Notebooks ~/notebooks
-    echo 'export SPARK_CLASSPATH="/dsvm/tools/spark/current/jars/azure-storage-2.0.0.jar,/dsvm/tools/spark/current/jars/hadoop-azure-2.7.4.jar,/home/laks/AML/jars/spark-avro_2.11-4.0.0.jar"' >> ~/.bashrc
-    echo 'export AZUREML_NATIVE_SHARE_DIRECTORY="/home/laks/mnt/azureml-share"' >> ~/.bashrc
-    echo "export TELEMETRY_STORAGE_ACCOUNT_NAME=$2" >> ~/.bashrc
-    echo "export TELEMETRY_STORAGE_ACCOUNT_KEY=$3" >> ~/.bashrc
-    echo "export STAGING_STORAGE_ACCOUNT_NAME=$2" >> ~/.bashrc
-    echo "export STAGING_STORAGE_ACCOUNT_KEY=$3" >> ~/.bashrc
-    echo 'export TELEMETRY_CONTAINER_NAME="telemetry""' >> ~/.bashrc
--- assets/install.sh
+++ assets/install.sh
@@ -17,13 +17,13 @@ sudo -u $username bash $homedir/envsetup.sh $1 $2 $3
 sudo cp $homedir/AML/config/spark-defaults.config /dsvm/tools/spark/current/conf/spark-defaults.conf
 sudo mkdir $homedir/mnt
 sudo mkdir $homedir/mnt/azureml-share
-sudo cp $homedir/AML/config/envvariable.sh /etc/profile.d/envvariable.sh
 sudo mount -t cifs //$2.file.core.windows.net/azureml-share $homedir/mnt/azureml-share -o vers=3.0,username=$2,password=$3,dir_mode=0777,file_mode=0777,serverino
 sudo chmod -R a+rwx $homedir/mnt/azureml-share
 
-    sudo echo 'export AZUREML_NATIVE_SHARE_DIRECTORY="/home/laks/mnt/azureml-share"' >> /etc/environment
-    sudo echo "export TELEMETRY_STORAGE_ACCOUNT_NAME=$2" >> /etc/environment
-    sudo echo "export TELEMETRY_STORAGE_ACCOUNT_KEY=$3" >> /etc/environment
-    sudo echo "export STAGING_STORAGE_ACCOUNT_NAME=$2" >> /etc/environment
-    sudo echo "export STAGING_STORAGE_ACCOUNT_KEY=$3" >> /etc/environment
-    sudo echo 'export TELEMETRY_CONTAINER_NAME="telemetry"' >> /etc/environment
+sudo echo 'os.environ["AZUREML_NATIVE_SHARE_DIRECTORY"]="/home/laks/mnt/azureml-share"' >> /etc/jupyterhub/jupyterhub_config.py
+sudo echo "os.environ['TELEMETRY_STORAGE_ACCOUNT_NAME']=\"$2\"" >> /etc/jupyterhub/jupyterhub_config.py
+sudo echo "os.environ['TELEMETRY_STORAGE_ACCOUNT_KEY']=\"$3\"" >> /etc/jupyterhub/jupyterhub_config.py
+sudo echo "os.environ['STAGING_STORAGE_ACCOUNT_NAME']=\"$2\"" >> /etc/jupyterhub/jupyterhub_config.py
+sudo echo "os.environ['STAGING_STORAGE_ACCOUNT_KEY']=\"$3\"" >> /etc/jupyterhub/jupyterhub_config.py
+sudo echo 'os.environ["TELEMETRY_CONTAINER_NAME"]="telemetry"' >> /etc/jupyterhub/jupyterhub_config.py
+sudo echo 'c.Spawner.env_keep.extend(["AZUREML_NATIVE_SHARE_DIRECTORY", "TELEMETRY_STORAGE_ACCOUNT_NAME", "TELEMETRY_STORAGE_ACCOUNT_KEY", "STAGING_STORAGE_ACCOUNT_NAME", "STAGING_STORAGE_ACCOUNT_KEY", "TELEMETRY_CONTAINER_NAME"])' >> /etc/jupyterhub/jupyterhub_config.py
