5fa9230cb02bde2a32f930c4a97308a070f8f43e
Lakshmanan Ramu Meenal <laramume@microsoft.com>
2018-05-15 17:07:32 -0700

    updayed dsvm

--- src/setup_scripts/envsetup.sh
+++ src/setup_scripts/envsetup.sh
@@ -7,7 +7,7 @@ popd
 
 source /anaconda/bin/activate py35
 conda install nbformat
-conda env update --file ~/AML/aml_config/conda_dependencies.yml
+conda env update --file ~/notebooks/aml_config/conda_dependencies.yml
 
 mkdir ~/mnt
 mkdir ~/mnt/azureml-share
