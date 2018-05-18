131a766cbaa1dbb6268c8883f97468a62e25035c
Lakshmanan Ramu Meenal <laramume@microsoft.com>
2018-05-18 11:12:04 -0700

    notebook directory fix

--- assets/envsetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-source /anaconda/bin/activate py35
-conda install nbformat
-conda env update --file ~/notebooks/aml_config/conda_dependencies.yml
-
-mkdir ~/mnt
-mkdir ~/mnt/azureml-share
