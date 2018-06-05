d51361585fc495e9e501692002a229ad425beaef
Andrew <anivan@microsoft.com>
2018-06-04 17:02:53 -0700

    more operationalization

--- src/CustomScripts/envsetup.sh
+++ src/CustomScripts/envsetup.sh
@@ -12,5 +12,7 @@ conda env update --file ~/notebooks/aml_config/conda_dependencies.yml
 conda install -n py35 -y python-snappy
 pip install imblearn
 
+pip install --upgrade --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-release/Preview/E7501C02541B433786111FE8E140CAA1 azureml-sdk
+
 mkdir ~/mnt
 mkdir ~/mnt/azureml-share
