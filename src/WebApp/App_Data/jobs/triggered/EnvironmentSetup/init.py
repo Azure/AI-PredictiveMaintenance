aeeed8f8d19944957493d4586e43062efe3d784f
Andrew <anivan@microsoft.com>
2018-06-06 16:25:43 -0700

    cleanup. removing unused workbench dependencies

--- src/WebApp/App_Data/jobs/triggered/EnvironmentSetup/init.py
+++ src/WebApp/App_Data/jobs/triggered/EnvironmentSetup/init.py
@@ -12,35 +12,10 @@ STORAGE_ACCOUNT_KEY = os.environ['STORAGE_ACCOUNT_KEY']
 
 table_service = TableService(account_name=STORAGE_ACCOUNT_NAME, account_key=STORAGE_ACCOUNT_KEY)
 
-table_service.create_table('cluster')
-
 file_service = FileService(account_name=STORAGE_ACCOUNT_NAME, account_key=STORAGE_ACCOUNT_KEY)
-file_service.create_share(share_name='azureml-project', quota=1)
-file_service.create_share(share_name='azureml-share', quota=1)
-
-file_service.create_directory('azureml-share', 'Solution1')
-file_service.create_directory('azureml-share', 'Solution2')
+file_service.create_share(share_name='notebooks', quota=1)
 
 block_blob_service = BlockBlobService(account_name=STORAGE_ACCOUNT_NAME, account_key=STORAGE_ACCOUNT_KEY)
 
 container_name ='telemetry'
-block_blob_service.create_container(container_name)  
-
-source=os.environ['AML_ASSETS_URL']
-dest='azureml_project.zip'
-
-urllib.request.urlretrieve(source, dest)
-
-with zipfile.ZipFile(dest,"r") as zip_ref:
-    zip_ref.extractall("azureml-project")
-
-for root, dirs, files in os.walk('azureml-project', topdown=True):
-    directory = os.path.relpath(root, 'azureml-project')
-    if directory != '.':
-        file_service.create_directory('azureml-project', directory)
-    for f in files:
-        file_service.create_file_from_path(
-            'azureml-project',
-            directory,
-            f,
-            os.path.join(root, f))
+block_blob_service.create_container(container_name)
