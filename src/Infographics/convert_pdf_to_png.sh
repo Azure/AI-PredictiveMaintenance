72f69e71bbd8c101f9dbbe2ae6b939d1d8911edd
Andrew Ivanov <andrew.ivanov@live.com>
2018-06-26 22:38:34 -0700

    fixing the blur

--- src/Infographics/convert_pdf_to_png.sh
+++ src/Infographics/convert_pdf_to_png.sh
@@ -2,7 +2,7 @@
 
 convert -density 150 -background white -alpha remove data_flow.pdf -quality 90 data_flow.png
 convert -density 90 -background white -alpha remove data_collection.pdf -quality 90 data_collection.png
-convert -density 90 -background white -alpha remove modeling.pdf -quality 90 modeling.png
+convert -density 150 -background white -alpha remove modeling.pdf -quality 90 modeling.png
 convert -density 90 -background white -alpha remove productionalization_feature_engineering.pdf -quality 90 productionalization_feature_engineering.png
 convert -density 90 -background white -alpha remove productionalization_scoring.pdf -quality 90 productionalization_scoring.png
 
