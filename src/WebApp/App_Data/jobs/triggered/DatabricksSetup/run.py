bec983cf655fb869e4315f3d7ec077862a3c982f
Lakshmanan Ramu Meenal <laramume@microsoft.com>
2018-06-27 21:07:20 -0700

    databricks integration

--- src/WebApp/App_Data/jobs/triggered/DatabricksSetup/run.py
+++ src/WebApp/App_Data/jobs/triggered/DatabricksSetup/run.py
@@ -5,6 +5,7 @@ import requests
 import uuid
 import json
 import zipfile
+import base64
 from azure.storage.table import TableService, Entity, TablePermissions
 
 STORAGE_ACCOUNT_NAME = os.environ['STORAGE_ACCOUNT_NAME']
@@ -63,13 +64,36 @@ def upload_notebooks_databricks():
 
     #upload feature engineering notebook to databricks workspace
     featureEngineering_local_path = os.path.join(notebooks_local_path, 'FeatureEngineering.ipynb')
-    print(featureEngineering_local_path)
     files = {'file': open(featureEngineering_local_path, 'rb')}
     bdfs = "/FeatureEngineering"
     put_payload = { 'path' : bdfs, 'overwrite' : 'true', 'language':'PYTHON', 'format':'JUPYTER' }
     resp = call_api('2.0/workspace/import', method=requests.post, data=put_payload, files = files).json()
 
+    #upload data ingestion notebook to databricks workspace
+    dataIngestion_local_path = os.path.join(notebooks_local_path, 'DataIngestion.ipynb')
+    files = {'file': open(dataIngestion_local_path, 'rb')}
+    bdfs = "/DataIngestion"
+    put_payload = { 'path' : bdfs, 'overwrite' : 'true', 'language':'PYTHON', 'format':'JUPYTER' }
+    resp = call_api('2.0/workspace/import', method=requests.post, data=put_payload, files = files).json()
+
 upload_notebooks_databricks()
+data = '{"DataIngestion" : { "STORAGE_ACCOUNT_NAME" :"' + STORAGE_ACCOUNT_NAME + '", "STORAGE_ACCOUNT_KEY" :"' + STORAGE_ACCOUNT_KEY +'", "TELEMETRY_CONTAINER_NAME" : "telemetry", "LOG_TABLE_NAME" : "Logs", "DATA_ROOT_FOLDER" : "/dbfs/root"}}'
+
+file = open('D:/home/site/NotebookEnvironmentVariablesConfig.json','w')
+file.write(data)
+file.close()
+    
+config_path = '/root/NotebookEnvironmentVariablesConfig.json'
+files = {'file': open('D:/home/site/NotebookEnvironmentVariablesConfig.json', 'rb')}
+put_payload = { 'path' : config_path, 'overwrite' : 'true' }
+
+call_api('2.0/dbfs/put', method=requests.post, data=put_payload, files=files)
+
+#handle = call_api("2.0/dbfs/create", method=requests.post, json = {"path": "/root/NotebookEnvironmentVariablesConfig.json", "overwrite": "true"}).json()['handle']
+#data_bytes = base64.b64encode(data.encode())
+#call_api("2.0/dbfs/add-block",method=requests.post, json = {"handle": handle, "data": data})
+# close the handle to finish uploading
+#call_api("2.0/dbfs/close",method=requests.post, json = {"handle": handle})
 
 last_run_id = get_last_run_id()
 
@@ -84,6 +108,9 @@ jar_dbfs_path = dbfs_path + 'featurizer_2.11-1.0.jar'
 urllib.request.urlretrieve(FEATURIZER_JAR_URL, jar_local_path)
 
 mkdirs_payload = { 'path': dbfs_path }
+call_api('2.0/dbfs/mkdirs', method=requests.post, json=mkdirs_payload)
+
+mkdirs_payload = { 'path': 'root/data/logs' }
 call_api('2.0/dbfs/mkdirs', method=requests.post, json=mkdirs_payload)
 
 files = {'file': open(jar_local_path, 'rb')}
