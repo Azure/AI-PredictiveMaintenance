f05c7ecba8fe7fb21aa0c0147c0f9979b72de987
Lakshmanan Ramu Meenal <laramume@microsoft.com>
2018-08-06 14:48:55 -0700

    deploy from github

--- src/WebApp/App_Data/jobs/continuous/PythonSetup/run.cmd
+++ src/WebApp/App_Data/jobs/continuous/PythonSetup/run.cmd
@@ -3,8 +3,9 @@ set READY_FILE=D:\home\site\READY
 IF EXIST %READY_FILE% EXIT
 
 mkdir D:\home\site\jars
-D:\home\python364x64\python.exe -m pip install --upgrade pip
+D:\home\python364x64\python.exe -m pip install pip
 D:\home\python364x64\python.exe -m pip install --upgrade -r D:\home\site\wwwroot\requirements.txt
 D:\home\python364x64\python.exe -m pip install D:\home\site\wwwroot\shared_modules
+D:\home\python364x64\python.exe run.py
 echo. > %READY_FILE%
 EXIT %errorlevel%
