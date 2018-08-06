613e86e4e1891278b4daa81d727ce85862e00d37
Lakshmanan Ramu Meenal <laramume@microsoft.com>
2018-08-06 15:52:49 -0700

    deploy from github

--- src/WebApp/App_Data/jobs/continuous/DatabricksAndSimulatedDevicesSetup/run.cmd
+++ src/WebApp/App_Data/jobs/continuous/DatabricksAndSimulatedDevicesSetup/run.cmd
@@ -1,7 +1,10 @@
 set READY_FILE=D:\home\site\READY
+set FINISH_FILE=D:\home\site\FINISH
 
+IF NOT EXIST %FINISH_FILE% EXIT
 IF NOT EXIST %READY_FILE% EXIT
 
 D:\home\python364x64\python.exe simulated_devices_setup.py
 D:\home\python364x64\python.exe run.py
+echo. > %FINISH_FILE%
 
